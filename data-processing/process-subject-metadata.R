c7d67a81bfd2749e56612acbd27ef3808b889e97
Josh de Leeuw <josh.deleeuw@gmail.com>
2024-01-01 16:58:08 -0500

    add prolific check, post to OSF

--- data-processing/process-subject-metadata.R
+++ data-processing/process-subject-metadata.R
@@ -9,6 +9,10 @@ osf_access_token <- paste(
 private_json_osf_component <- "vmdu6"
 private_jpg_osf_component <- "3b6et"
 private_webm_osf_component <- "34t8d"
+private_osf_overall_component <- "2wcgp"
+
+public_jpg_osf_component <- "7s2ae"
+public_json_osf_component <- "6cn7a"
 #######################
 
 library(osfr)
@@ -45,6 +49,11 @@ json_data <- list.files(
   lapply(fromJSON) %>%
   bind_rows()
 
+prolific_info <- json_data %>%
+  select(subject_id, prolific_id) %>%
+  group_by(subject_id) %>%
+  slice_head(n = 1)
+
 data_trials <- c(
   "webcam-type",
   "glasses",
@@ -55,6 +64,36 @@ data_trials <- c(
 
 experiment_data <- json_data %>%
   filter((task %in% data_trials) | (trial_type == "html-video-response"))
+
+people_who_give_permission_for_videos <- experiment_data %>%
+  filter(task == "public-videos") %>%
+  select(subject_id, response) %>% 
+  filter(response == 0) %>%
+  pull(subject_id)
+
+jpg_files <- list.files(
+  path = "data/jpg",
+  pattern = ".jpg$",
+)
+
+jpg_files_per_subject <- data.frame(file_name = jpg_files) %>%
+  mutate(subject_id = substring(file_name, 1, 8)) %>%
+  group_by(subject_id) %>%
+  count()
+
+subjects_with_complete_data <- jpg_files_per_subject %>%
+  filter(n == 144) %>%
+  pull(subject_id)
+
+subjects_with_complete_data_and_permission <- subjects_with_complete_data %>%
+  intersect(people_who_give_permission_for_videos)
+
+writeLines(toJSON(
+  subjects_with_complete_data_and_permission
+), "data/subjects-with-permission.json")
+
+osf_retrieve_node(private_osf_overall_component) %>%
+  osf_upload("data/subjects-with-permission.json", conflicts="overwrite")
 
 webcam_info <- experiment_data %>%
   filter(task == "webcam-type") %>%
@@ -84,16 +123,6 @@ screen_info <- experiment_data %>%
   filter(task == "browser-check") %>%
   select(subject_id, aspect_ratio, width, height)
 
-jpg_files <- list.files(
-  path = "data/jpg",
-  pattern = ".jpg$",
-)
-
-jpg_files_per_subject <- data.frame(file_name = jpg_files) %>%
-  mutate(subject_id = substring(file_name, 1, 8)) %>%
-  group_by(subject_id) %>%
-  count()
-
 detection_task_data <- json.data %>%
   filter(task == "dot_detection") %>%
   select(rt, subject_id) %>%
@@ -107,11 +136,6 @@ detection_task_data <- json.data %>%
     "prop<3200ms" = sum(rt < 3200) / n()
   )
 
-interacts.data <- NULL
-
-# Concatenate json files into one megafile
-
-# Creates a list of files from json directory
 interaction_data <- list.files(
   path = "data/json",
   pattern = "_interactions.json$",
@@ -132,10 +156,24 @@ interaction_data_wide <- interaction_data %>%
 
 interaction_data_wide[is.na(interaction_data_wide)] <- 0
 
-
 fullscreenexit_trial <- interaction_data %>%
   filter(event == "fullscreenexit") %>%
   group_by(subject_id) %>%
   slice_head(n = 1) %>%
   select(subject_id, trial) %>%
   rename("fullscreen_exit_trial" = "trial")
+
+### create filtered jpg.tar.gz
+
+public_jpg_files <- jpg_files[str_extract(jpg_files, "^[^_]+")  %in% subjects_with_complete_data_and_permission]
+
+tar(
+  tarfile = "data/public-jpg.tar.gz",
+  files = file.path("data/jpg", public_jpg_files),
+  compression = "gzip"
+)
+
+osf_retrieve_node(public_jpg_osf_component) %>%
+  osf_upload("data/public-jpg.tar.gz", conflicts="overwrite")
+
+
